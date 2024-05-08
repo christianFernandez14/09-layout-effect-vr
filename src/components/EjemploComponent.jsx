bae4ecca5704f44ecddccb7db5f9f314bab174c3
Christian Fernandez <christian14@gmail.com>
2024-05-07 20:38:49 -0400

    Una manera de trabjar el estado con un flag y pasando un parametro previo (prev) .134-2

--- src/components/EjemploComponent.jsx
+++ src/components/EjemploComponent.jsx
@@ -1,32 +1,42 @@
-import React, { useEffect, useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 
 export const EjemploComponent = () => {
 
+  const [mostrar, setMostrar] = useState(false)
 
 
   // Este lo hara luego de que este todo renderizado
   useEffect(() => {
     console.log('useEffect: Componente Cargado')
-    let caja = document.querySelector('#caja')
-    // Aca se escribio mal la palabra para que se viera que se esta es el que predomina.
-    caja.innerHTML = 'HOLLA 2'
+
   }, [])
 
 
   // No importa donde lo coloques en el codigo, se imprimera primero lo que haya dentro de este hook
   useLayoutEffect(() => {
     console.log('useLayoutEffect: Componente Cargado')
-    let caja = document.querySelector('#caja')
-    caja.innerHTML = 'HOLAA'
-    // Este metodo me trae todo las coordenadas de la caja, ya que ella se la tomo del DOM
-    console.log(caja.getBoundingClientRect())
+
   }, [])
 
 
   return (
     <div>
       <h1>Ejemplo useEffect y useLayoutEffect</h1>
-      <div id="caja"></div>
+
+      {/* De esta manera puedo hacer un Flag muy facil para mostrar y dejar de mostrar algo*/}
+      <button onClick={() => setMostrar(prev => {
+        // le metimos un clg para que veas el comportamiento del flag, pero puedes quitar los corchetes y dejar implicito
+        console.log(!prev)
+        return !prev
+      })}>Mostrar mensaje</button>
+
+      {mostrar && (
+        <div id="caja">
+          Hola soy un mensaje {mostrar}
+        </div>
+
+      )
+      }
 
     </div>
   )
